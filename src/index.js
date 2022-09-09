5a4ab06bda33f5607e7a52a4f209d5d164e2727f
maarig <mmaarig@gmail.com>
2022-09-09 22:58:39 +0500

    2nd Edit

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ function Hi() {
         <i>Name</i>
       </strong>{" "}
       is <i>italic</i> and <u>underlined</u>. And this is a <h1>Heading</h1>. */}
-      <h1>Resume</h1>
-      <p><b>Name :</b> Muhammad Maarig</p>
+      <br/><h1>Resume</h1><hr/>
+      <p><b>Name :</b> Muhammad Maarig</p><br/>
       <p><b>Experiance </b></p> 
-      <p>Teaching - 06 Years</p>
+      <p>Teaching - 06 Years</p><br/>
       <p><b>Skills</b></p>
       <p>01 - Teaching</p>
       <p>02 - Front-End Development</p>
       <p>03 - Content Writing</p>
-      <p>04 - Digital Marketing</p>
+      <p>04 - Digital Marketing</p><br/>
       <p><b>Qualification :</b></p>
-      <p>Graduation</p>
+      <p>Graduation</p><br/>
       <p><b>Contact</b></p>
       <p><a href="https://github.com/mmaarig">Github</a></p>
       <p><a href="https://www.linkedin.com/in/mmaarig/">LinkedIn</a></p>
